7d1054f370913a7b41d8af740b860e2061cd5b6b
Edward <edward.maesen@gmail.com>
2020-10-13 14:37:59 -0700

    Add basic config

--- webapp/gridsome.config.js
+++ webapp/gridsome.config.js
@@ -5,6 +5,15 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Vue-Gridsome-Strapi-concept',
+  siteUrl: '',
+  titleTemplate: '%s',
+  metadata: {
+    version: 'V0.1.0',
+  },
+  icon: {
+    favicon: './src/assets/images/icons/favicon.png',
+    touchicon: './src/assets/images/icons/touch-icon.png'
+  },
   plugins: []
 }
