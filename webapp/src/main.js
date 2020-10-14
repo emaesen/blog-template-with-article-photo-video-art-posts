ae46f2c198414fc182782da978fbb4c365a35b67
Edward <edward.maesen@gmail.com>
2020-10-14 08:56:23 -0700

    Install test font

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,7 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-require('fontsource-josefin-sans');
+require('fontsource-questrial');
 
 import DefaultLayout from '~/layouts/Default.vue'
 
