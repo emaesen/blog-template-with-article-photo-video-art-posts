be700c6bf231cd938a60139fa5bcac00b1decc9f
Edward <edward.maesen@gmail.com>
2020-10-20 16:53:39 -0700

    change env variable

--- webapp/src/utils/medias.js
+++ webapp/src/utils/medias.js
@@ -2,7 +2,7 @@ export function getCmsMedia(url) {
   // Check if URL is a local path
   if (url.startsWith('/')) {
     // Prepend Cms address
-    const cmsUrl = process.env.GRIDSOME_CMS_URL
+    const cmsUrl = process.env.GRIDSOME_CMS_MEDIA_PATH
     return cmsUrl + url
   }
   // Otherwise return full URL
