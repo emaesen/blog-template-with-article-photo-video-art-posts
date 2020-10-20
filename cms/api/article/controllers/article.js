7de55f8545230fa9be9e85452bd31e4e1b30bb0c
Edward <edward.maesen@gmail.com>
2020-10-20 16:30:12 -0700

    sanitize article results

--- cms/api/article/controllers/article.js
+++ cms/api/article/controllers/article.js
@@ -5,4 +5,22 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.article.findOne({ slug });
+    // `sanitizeEntity` removes all private fields from the model 
+    // and its relations
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+};
+
