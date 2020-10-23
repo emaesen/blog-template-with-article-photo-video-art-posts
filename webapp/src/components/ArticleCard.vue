c2ef60df3098b2f82999bed8282ac3531619b655
Edward <edward.maesen@gmail.com>
2020-10-23 11:38:57 -0700

    add styling classes

--- webapp/src/components/ArticleCard.vue
+++ webapp/src/components/ArticleCard.vue
@@ -1,43 +1,36 @@
 <template>
-  <div class="">
+  <div class="article-card">
+    <div class="article-summary">
+      <g-link :to="'article/' + article.slug">
+        <h3 class="">
+          {{ article.title }}
+        </h3>
+      </g-link>
+      <g-image
+        :alt="article.title"
+        :src="getCmsMedia(article.coverImage.url)"
+        class="article-cover-image"
+      />
+      <div class="article-categories">
+        <div
+          v-for="category in article.categories"
+          :key="category.id"
+          class="article-category"
+        >
+          {{ category.title }}
+        </div>
+      </div>
+      <p class="article-description">
+        {{ article.description }}
+      </p>
+    </div>
     <g-link
       :to="'article/' + article.slug"
-      class=""
+      :title="article.title"
+      class="article-link"
     >
-    <g-image
-      :alt="article.title"
-      :src="getCmsMedia(article.coverImage.url)"
-      class=""
-    />
+      Read more...
     </g-link>
-    <div class="">
-      <div>
-        <g-link :to="'article/' + article.slug">
-          <h3 class="">
-            {{ article.title }}
-          </h3>
-        </g-link>
-        <div class="">
-          <div
-            v-for="category in article.categories"
-            :key="category.id"
-            class=""
-          >
-            {{ category.title }}
-          </div>
-        </div>
-        <p>
-          {{ article.description }}
-        </p>
-      </div>
-      <g-link
-        :to="'article/' + article.slug"
-        :title="article.title"
-        class=""
-      >
-        View article 👉
-      </g-link>
-    </div>
   </div>
 </template>
 
@@ -51,3 +44,13 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+
+.article-card {
+  margin: 2em 0;
+  padding: .5em 2em;
+  border: 1px solid #ccc;
+  background-color: #eee;
+}
+</style>
