84d1194e6d17ed743e37065bd7b1628d77dfed0c
Edward <edward.maesen@gmail.com>
2020-10-26 09:10:28 -0700

    load remote cms images

--- webapp/gridsome.server.js
+++ webapp/gridsome.server.js
@@ -3,11 +3,40 @@
 // Learn more: https://gridsome.org/docs/server-api/
 
 // Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `npm run dev`
+// To stop current process, press CTRL + C in terminal.
+
+const axios = require('axios')
+
+async function populateCmsImagesCollection(cmsImagesCollection) {
+  const imagesResponse = await axios.get(process.env.GRIDSOME_CMS_IMAGES_URL);
+  if (imagesResponse && imagesResponse.status === 200) {
+    const imagesJson = imagesResponse.data
+    console.log(`INFO: Received metadata for ${imagesJson.length} CMS images`)
+    for(let img of imagesJson) {
+      // TODO: decide if to add criteria to exclude some of the images
+      //       from being processed
+      // (I haven't found a conclusive way to determine if an image is
+      //  actually referenced anywhere in the CMS content - particularly
+      //  images embedded inside Rich Text)
+      if(img.url) {
+        cmsImagesCollection.addNode(img)
+        console.log(`INFO: added image ${img.name} to cmsImagesCollection`)
+      } else {
+        console.log(`INFO: excluded image ${img.name} from cmsImagesCollection`)
+      }
+    }
+  } else {
+    console.error("ERROR: Unable to fetch CMS images")
+  }
+}
 
 module.exports = function (api, options) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async ({ addCollection }) => {
     // Data Store API docs: https://gridsome.org/docs/data-store-api/
+
+    await populateCmsImagesCollection(addCollection('CmsImages'))
+    
+
   })
 
   api.createPages(async ({ createPage, graphql }) => {
