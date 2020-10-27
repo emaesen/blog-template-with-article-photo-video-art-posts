fe37a0b446a33842ae2041cb9f0a69258bcd44c0
Edward <edward.maesen@gmail.com>
2020-10-27 13:02:27 -0700

    download cms media files

--- webapp/gridsome.server.js
+++ webapp/gridsome.server.js
@@ -6,14 +6,24 @@
 // To stop current process, press CTRL + C in terminal.
 
 const axios = require('axios').default
+const path = require('path')
+const fse = require('fs-extra')
+
+const CMS_URL = process.env.CMS_URL
+const CMS_MEDIA_URL = process.env.CMS_MEDIA_URL
+const CMS_MEDIA_TARGET_PATH = path.join(process.cwd(), 'src', process.env.GRIDSOME_CMS_MEDIA_PATH)
+
+function getMediaFilename(media) {
+  return media.hash + media.ext
+}
 
 async function populateCmsMediaCollections(addCollection) {
   let tally = {count:0, mediaFiles:[]}
   let mediaResponse
   try {
-    mediaResponse = await axios.get(process.env.CMS_MEDIA_URL)
+    mediaResponse = await axios.get(CMS_MEDIA_URL)
   } catch (error) {
-    console.error(`ERROR: ${error.response.statusText} error retrieving ${process.env.CMS_MEDIA_URL}`)
+    console.error(`ERROR: ${error.response.statusText} error retrieving ${CMS_MEDIA_URL}`)
   }
 
   if (mediaResponse && mediaResponse.status === 200) {
@@ -26,14 +36,14 @@ async function populateCmsMediaCollections(addCollection) {
       })
       return simplifiedAsset
     })
-    console.info(`INFO: Received metadata for ${mediaJson.length} CMS media assets from  ${process.env.CMS_MEDIA_URL}`)
+    console.info(`INFO: Received metadata for ${mediaJson.length} CMS media assets from  ${CMS_MEDIA_URL}`)
     tally = await populateCmsMediaCollection(mediaJson, "image", addCollection('CmsImages'), tally)
     tally = await populateCmsMediaCollection(mediaJson, "video", addCollection('CmsVideos'), tally)
     tally = await populateCmsMediaCollection(mediaJson, "audio", addCollection('CmsAudios'), tally)
   } else {
     console.error("ERROR: Unable to process CMS media assets metadata")
   }
-  console.log("External files to retrieve:", tally.mediaFiles)
+  console.log(`Files to retrieve from ${CMS_URL}:`, tally.mediaFiles)
   return tally.mediaFiles
 }
 
@@ -49,8 +59,12 @@ async function populateCmsMediaCollection(mediaJson, type, cmsCollection, tally)
     //  actually referenced anywhere in the CMS content - particularly
     //  media assets embedded inside Rich Text)
     if(media.mime.startsWith(type)) {
+      // add absolute local path property
+      media.localImage = path.resolve(CMS_MEDIA_TARGET_PATH, getMediaFilename(media))
+      // add media data to the collection
       cmsCollection.addNode(media)
       console.info(`INFO: ${count} CMS ${type} Collection: added ${media.name}`)
+      // populate mediaFiles aray with all available media files
       mediaFiles.push(media.url)
       if (media.formats) {
         // also include alternative formats of the asset
@@ -65,10 +79,44 @@ async function populateCmsMediaCollection(mediaJson, type, cmsCollection, tally)
   return {count, mediaFiles};
 }
 
+async function downloadCmsMediaFile(file) {
+  // remove any subfolder
+  const targetFile = path.join(CMS_MEDIA_TARGET_PATH, file.replace(/\/.*\//,""))
+
+  // download the CMS media file, unless it exists already
+  if (await fse.exists(targetFile)) {
+    console.info("INFO: file exists " + targetFile)
+  } else {
+    try {
+      response = await axios({
+        method: 'get',
+        url: CMS_URL + file,
+        responseType: 'stream'
+      })
+      response.data.pipe(fse.createWriteStream(targetFile))
+      console.info("INFO: created " + targetFile)
+    } catch (error) {
+      console.error(`ERROR: error retrieving ${file}`)
+      console.error(error)
+    }
+  }
+}
+
+async function downloadCmsMediaFiles(files) {
+  await files.forEach(async file => {
+    await downloadCmsMediaFile(file)
+  })
+}
+
 module.exports = function (api, options) {
   api.loadSource(async ({ addCollection }) => {
     // Data Store API docs: https://gridsome.org/docs/data-store-api/
-    await populateCmsMediaCollections(addCollection)
+    const mediaFiles = await populateCmsMediaCollections(addCollection)
+    await downloadCmsMediaFiles(mediaFiles)
+  })
+
+  api.onCreateNode(options => {
+    console.log("LOG: onCreateNode",{options})
   })
 
   api.createPages(async ({ createPage, graphql }) => {
