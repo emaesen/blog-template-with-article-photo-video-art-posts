3d9c718bb6e8dedf868d945ffd201bc9481eff71
Edward <edward.maesen@gmail.com>
2020-11-02 15:27:33 -0800

    tweak footer layout, styling and content

--- webapp/src/components/Footer.vue
+++ webapp/src/components/Footer.vue
@@ -1,51 +1,26 @@
 <template>
-  <footer class="">
-    <p class="">
-      {{ $static.cms.global.siteName }}
-    </p>
-    <div class="">
-      <!-- Site name, contact email and copyright -->
-      <div class="">
-        <p>{{ $static.cms.global.contactEmail }}</p>
-        <p class="">
-          Copyright {{ new Date().getFullYear() }}
-        </p>
-      </div>
-      <!-- Site pages -->
-      <ul class="">
-        <li
-          v-for="page in pages" 
-          :key="page.path"
-        >
-          <g-link
-            :to="page.path"
-            class=""
-          >
-            {{ page.title }}
-          </g-link>
-        </li>
-      </ul>
-      <!-- Social network links -->
-      <ul class="">
-        <li
-          v-for="socialNetwork in $static.cms.global.socialNetworks" 
-          :key="socialNetwork.id"
-        >
-          <a
-            :href="socialNetwork.url"
-            target="_blank"
-            rel="noopener noreferrer"
-            class=""
-          >
-            <img
-              :src="getCmsMedia(socialNetwork.icon.url)"
-              :alt="socialNetwork.title"
-              class=""
-            />
-            <span>{{ socialNetwork.title }}</span>
-          </a>
-        </li>
-      </ul>
+  <footer>
+    <!-- Social network links -->
+    <h6>{{ $static.cms.global.socialNetworksHeader }}</h6>
+    <ul class="social">
+      <li
+        v-for="socialNetwork in $static.cms.global.socialNetworks" 
+        :key="socialNetwork.id"
+      >
+        <a
+          :href="socialNetwork.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          class=""
+        >{{ socialNetwork.title }}</a>
+      </li>
+    </ul>
+
+    <div class="legal layout-width">
+      <span class="contact">{{ $static.cms.global.contactEmail }}</span>
+      <span class="copyright">
+        © {{ copyrightYear }} {{ $static.cms.global.siteName }}. {{ $static.cms.global.rightsText }}
+      </span>
     </div>
   </footer>
 </template>
@@ -59,9 +34,6 @@ export default {
   },
   computed: {
     pages() {
-      // Select random project index
-      const { projects } = this.$static.cms
-      const randomIndex = Math.floor(Math.random() * projects.length)
       return [
         {
           title: 'Home',
@@ -71,36 +43,66 @@ export default {
           title: 'About',
           path: '/about',
         },
-        {
-          title: 'Random project',
-          path: `/project/${projects[randomIndex].slug}`,
-        }
       ]
     },
+    copyrightYear() {
+      const currentYear = new Date().getFullYear();
+      const startYear = this.$static.cms.global.createdAt.substring(0,4)
+      // TODO: add createAt as starting year in range
+      return startYear < currentYear ? startYear + " - " + currentYear : currentYear
+    }
   }
 }
 </script>
 
 <static-query>
-query {
+query Footer {
   cms {
     global {
+      createdAt
+      rightsText
       siteName
       contactEmail
+      socialNetworksHeader
       socialNetworks {
         id
         url
         title
-        icon {
-          id
-          url
-        }
       }
-    }
-    projects {
-      id
-      slug
     }
   }
 }
 </static-query>
+
+<style lang="less" scoped>
+ul.social {
+  margin-bottom: 3em;
+  list-style-type: none;
+  margin-block-start:0;
+  margin-block-end:3em;
+  margin-inline-start:0;
+  margin-inline-end:0;
+  padding-inline-start: 0;
+}
+ul.social li {
+  display: inline;
+  margin-right: 2em;
+}
+.social-icon {
+  max-width: 20px;
+  margin-right: .2em;
+  vertical-align: top;
+}
+.legal {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  margin: auto;
+  text-align: center;
+  font-size: .75em;
+  padding-top: 5px;
+  border-top: 1px solid #0f5ca0;
+  z-index: 999;
+  background-color:#f5f2eadd;
+}
+</style>
