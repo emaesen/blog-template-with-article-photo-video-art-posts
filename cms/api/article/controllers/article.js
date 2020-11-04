46165b467a6322908adbabbadab22f27e1dad5ff
Edward <edward.maesen@gmail.com>
2020-11-04 15:28:41 -0800

    sanitize api response

--- cms/api/article/controllers/article.js
+++ cms/api/article/controllers/article.js
@@ -5,6 +5,8 @@
  * to customize this controller
  */
 
+// `sanitizeEntity` removes all private fields from the model 
+// and its relations
 const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
@@ -16,11 +18,20 @@ module.exports = {
 
   async findOne(ctx) {
     const { slug } = ctx.params;
-
+    console.log("findOne article: " + slug)
     const entity = await strapi.services.article.findOne({ slug });
-    // `sanitizeEntity` removes all private fields from the model 
-    // and its relations
     return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+  async find(ctx) {
+    let entities;
+    console.log("find article: " + ctx.query.slug)
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.article }));
   },
 };
 
--- cms/api/category/controllers/category.js
+++ cms/api/category/controllers/category.js
@@ -5,4 +5,33 @@
  * to customize this controller
  */
 
-module.exports = {};
+// `sanitizeEntity` removes all private fields from the model 
+// and its relations
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { title } = ctx.params;
+    console.log("findOne category: " + title)
+    const entity = await strapi.services.category.findOne({ title });
+    return sanitizeEntity(entity, { model: strapi.models.category });
+  },
+  async find(ctx) {
+    let entities;
+    console.log("find category: " + ctx.query.title)
+    if (ctx.query._q) {
+      entities = await strapi.services.category.search(ctx.query);
+    } else {
+      entities = await strapi.services.category.find(ctx.query);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.category }));
+  },
+};
+
