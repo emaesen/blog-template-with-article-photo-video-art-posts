3f95f00cda8218e0e5fa3c29addbb4337921c898
Edward <edward.maesen@gmail.com>
2020-11-13 12:37:19 -0800

    Tweak content and style

--- webapp/src/components/Footer.vue
+++ webapp/src/components/Footer.vue
@@ -1,20 +1,30 @@
 <template>
   <footer>
-    <!-- Social network links -->
-    <h6>{{ $static.cms.global.socialNetworksHeader }}</h6>
-    <ul class="social">
-      <li
-        v-for="socialNetwork in $static.cms.global.socialNetworks" 
-        :key="socialNetwork.id"
+    <!-- Other website links -->
+    <div class="social">
+      <div
+        v-for="list in websiteLists"
+        :key="list.title"
       >
-        <a
-          :href="socialNetwork.url"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="nowrap"
-        >{{ socialNetwork.title }}</a>
-      </li>
-    </ul>
+        <h6>{{ list.title }}</h6>
+        <ul class="social">
+          <li
+            v-for="website in list.website" 
+            :key="website.title"
+          >
+            <a
+              :href="website.url"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="nowrap"
+            >{{ website.title }}</a>
+            <span class="description">
+              {{ website.description }}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
 
     <div class="legal expand bgcolor-opaque">
       <span class="copyright">
@@ -24,20 +34,6 @@
   </footer>
 </template>
 
-<script>
-export default {
-  methods: {
-  },
-  computed: {
-    copyrightYear() {
-      const currentYear = new Date().getFullYear();
-      const startYear = this.$static.cms.global.createdAt.substring(0,4)
-      return startYear < currentYear ? startYear + " - " + currentYear : currentYear
-    }
-  }
-}
-</script>
-
 <static-query>
 query Footer {
   cms {
@@ -46,38 +42,65 @@ query Footer {
       rightsText
       siteName
       contactEmail
-      socialNetworksHeader
-      socialNetworks {
-        id
-        url
+      otherWebsites {
         title
+        website {
+          title
+          url
+          description
+        }
       }
     }
   }
 }
 </static-query>
 
+<script>
+export default {
+  methods: {
+  },
+  computed: {
+    websiteLists() {
+      return this.$static.cms.global.otherWebsites
+    },
+    copyrightYear() {
+      const currentYear = new Date().getFullYear();
+      const startYear = this.$static.cms.global.createdAt.substring(0,4)
+      return startYear < currentYear ? startYear + " - " + currentYear : currentYear
+    },
+  }
+}
+</script>
+
 <style lang="less" scoped>
 footer {
   margin-top: 5em;
 }
+footer h6 {
+  margin-top: 1.5em;
+}
+div.social {
+  margin-bottom: 5em;
+}
 ul.social {
-  margin-bottom: 3em;
+  margin-bottom: 0;
   list-style-type: none;
   margin-block-start:0;
-  margin-block-end:3em;
+  margin-block-end:0;
   margin-inline-start:0;
   margin-inline-end:0;
   padding-inline-start: 0;
 }
 ul.social li {
-  display: inline;
+  display: block;
   margin-right: 2em;
+  position: relative;
 }
-.social-icon {
-  max-width: 20px;
-  margin-right: .2em;
-  vertical-align: top;
+.description {
+  margin-left: 1em;
+}
+.description::before {
+  content: " ≻ ";
 }
 .legal {
   position: fixed;
