8e24d68b0c4db165dce9c6db0b7f11cf498e4008
Edward <edward.maesen@gmail.com>
2020-11-20 13:25:46 -0800

    add usage comment

--- webapp/src/utils/event-bus.js
+++ webapp/src/utils/event-bus.js
@@ -2,3 +2,26 @@
 
 import Vue from 'vue';
 export const EventBus = new Vue();
+
+/*
+ * // import in component
+ * import { EventBus } from '~/utils/event-bus'
+ *
+ * // emit event in component
+ * EventBus.$emit('my-event-name', optionalPayload)
+ * 
+ * // listen for event in component
+ * const eventHandler = function(payload) {
+ *   // take action
+ * }
+ * EventBus.$on('my-event-name', eventHandler)
+ * 
+ * // remove handler/listener for event
+ * EventBus.$off('my-event-name', eventHandler)
+ * 
+ * // remove all listeners for event
+ * EventBus.$off('my-event-name')
+ * 
+ * // remove all listeners for all events
+ * EventBus.$off()
+ */
