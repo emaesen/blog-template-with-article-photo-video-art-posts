cb27306eae8f41f9f2d3018454616950644669fd
Edward <edward.maesen@gmail.com>
2020-11-24 12:06:48 -0800

    move intro animation from index page to app layout

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-layout">
+  <div class="app-layout"  ref="pagelayout">
     <header class="header">
       <g-link to="/" class="nodeco">
         <g-image alt="logo" src="~/assets/images/logo.png" width="65"/>
@@ -12,6 +12,24 @@
       </transition>
     </main>
     <Footer/>
+
+    <Reveal
+      ref="revealContainer"
+      class="os_reveal"
+      @reveal-done="onRevealDone"
+    >
+    </Reveal>
+    <AnimatedSVG
+      ref="signatureContainer"
+      v-if="signatureSVG"
+      v-html="signatureSVG"
+      svgId="signature"
+      :animate="animateSignature"
+      :class="['os_signature', fillviewportClassName]"
+      @animatedsvg-done="onAnimatedSVGDone"
+    >
+    </AnimatedSVG>
+
   </div>
 </template>
 
@@ -20,22 +38,108 @@ query {
   metadata {
     siteName
   }
+  cms {
+    scalarVectorGraphics(where: {valueOfIdAttributeInSvg: "signature"}) {
+      code
+    }
+
+  }
 }
 </static-query>
 
 <script>
 import SiteNav from '~/components/SiteNav'
 import Footer from '~/components/Footer'
+import AnimatedSVG from '~/components/AnimatedSVG'
+import Reveal from '~/components/Reveal'
+
+import caniuse from '@/mixins/caniuse'
+
+import { EventBus } from '~/utils/event-bus'
 
 export default {
+  name: 'App',
+  mixins: [caniuse],
   components: {
     SiteNav,
     Footer,
+    AnimatedSVG,
+    Reveal,
+  },
+  data() {
+    return {
+      fillviewportClassName: "fillviewport",
+      animateSignature: true,
+      disableMotion: false,
+    }
+  },
+  mounted() {
+    if (this.disableMotion) {
+      this.caniuse.motion = false
+    }
+    if(this.caniuse.motion) {
+      this.pageClassList.add(this.fillviewportClassName)
+      EventBus.$emit('start-animatedsvg')
+    } else {
+      this.animateSignature = false
+      this.placeSignature()
+      this.removeRevealContainer()
+    }
+  },
+  computed: {
+    pageEl() {
+      console.log({pagelayout:this.$refs.pagelayout})
+      return this.$refs.pagelayout
+    },
+    pageClassList() {
+      return this.pageEl.classList
+    },
+    signatureSVG() {
+      return this.$static.cms.scalarVectorGraphics[0].code
+    },
+  },
+  methods: {
+    onAnimatedSVGDone() {
+      EventBus.$emit('start-reveal')
+    },
+    onRevealDone() {
+      this.pageClassList.remove(this.fillviewportClassName)
+      this.removeRevealContainer()
+      this.placeSignature()
+    },
+    removeRevealContainer() {
+      this.$refs.revealContainer.$el.remove()
+    },
+    placeSignature() {
+      this.$refs.signatureContainer.$el.classList.remove(this.fillviewportClassName)
+    }
   },
 }
 </script>
 
 <style lang="less">
 @import "~/assets/styles/app.less";
+
+.app-layout.fillviewport {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden scroll;
+}
+.os_signature {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 15vw;
+  min-width: 100px;
+  transform-origin: top left;
+  transform: scale(1) translate(75px,15px);
+  transition: transform 2.5s ease;
+}
+.os_signature.fillviewport {
+  transform: scale(3) translate(5vw,5vh);
+}
 
 </style>
--- webapp/src/pages/Index.vue
+++ webapp/src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-index" ref="pagelayout">
+  <div class="main-index">
 
     <h1>{{ $page.cms.home.title }}</h1>
 
@@ -30,32 +30,12 @@
     <!-- Section with latest articles -->
     <LatestArticles/>
 
-    <Reveal
-      ref="revealContainer"
-      class="os_reveal"
-      @reveal-done="onRevealDone"
-    >
-    </Reveal>
-    <AnimatedSVG
-      ref="signatureContainer"
-      v-if="signatureSVG"
-      v-html="signatureSVG"
-      svgId="signature"
-      :animate="animateSignature"
-      :class="['os_signature', fillviewportClassName]"
-      @animatedsvg-done="onAnimatedSVGDone"
-    >
-    </AnimatedSVG>
-
   </div>
 </template>
 
 <page-query>
 query IndexPage {
   cms {
-    scalarVectorGraphics(where: {valueOfIdAttributeInSvg: "signature"}) {
-      code
-    }
     # Get global data
     global {
       author {
@@ -134,56 +114,25 @@ query IndexPage {
 <script>
 import LatestArticles from '~/components/LatestArticles'
 import Content from '~/components/Content'
-import AnimatedSVG from '~/components/AnimatedSVG'
-import Reveal from '~/components/Reveal'
 
-import caniuse from '@/mixins/caniuse'
-
-import { EventBus } from '~/utils/event-bus'
 import { getCmsMedia } from '~/utils/medias'
 import { getMetaTags } from '~/utils/meta-tags'
 
 
 export default {
-  name: 'home',
-  mixins: [caniuse],
+  name: 'Index',
   components: {
     LatestArticles,
     Content,
-    AnimatedSVG,
-    Reveal,
   },
   data() {
     return {
-      fillviewportClassName: "fillviewport",
-      animateSignature: true,
     }
   },
   metaInfo() {
     return getMetaTags(this.$page.cms.home.seo) 
   },
-  mounted() {
-    console.log({"location": document.location})
-    console.log({"URL": document.URL})
-    console.log({"referrer": document.referrer})
-    const internalReferrer = document.referrer.startsWith(document.URL)
-    if(!internalReferrer && this.caniuse.motion) {
-      this.pageClassList.add(this.fillviewportClassName)
-      EventBus.$emit('start-animatedsvg')
-    } else {
-      this.animateSignature = false
-      this.placeSignature()
-      this.removeRevealContainer()
-    }
-  },
   computed: {
-    pageEl() {
-      console.log({pagelayout:this.$refs.pagelayout})
-      return this.$refs.pagelayout
-    },
-    pageClassList() {
-      return this.pageEl.classList
-    },
     global() {
       return this.$page.cms.global
     },
@@ -199,54 +148,14 @@ export default {
     organizations() {
       return this.global.organization
     },
-    signatureSVG() {
-      return this.$page.cms.scalarVectorGraphics[0].code
-    },
     location() {
       return this.address.city + ", " + this.address.state_province + ", " + this.address.country
-    }
-  },
-  methods: {
-    onAnimatedSVGDone() {
-      EventBus.$emit('start-reveal')
-    },
-    onRevealDone() {
-      this.pageClassList.remove(this.fillviewportClassName)
-      this.removeRevealContainer()
-      this.placeSignature()
-    },
-    removeRevealContainer() {
-      this.$refs.revealContainer.$el.remove()
-    },
-    placeSignature() {
-      this.$refs.signatureContainer.$el.classList.remove(this.fillviewportClassName)
     }
   },
 }
 </script>
 
 <style lang="less" scoped>
-.layout.fillviewport {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden scroll;
-}
-.os_signature {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 15vw;
-  min-width: 100px;
-  transform-origin: top left;
-  transform: scale(1) translate(75px,15px);
-  transition: transform 2.5s ease;
-}
-.os_signature.fillviewport {
-  transform: scale(3) translate(5vw,5vh);
-}
 .photo {
   width: 20%;
   max-width: 100px;
