9df495e86133044245b52f866847aa8da6c062c4
Edward <edward.maesen@gmail.com>
2020-11-24 15:02:38 -0800

    move opening animation to component

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-layout"  ref="pagelayout">
+  <div class="app-container">
     <header class="header">
       <g-link to="/" class="nodeco">
         <g-image alt="logo" src="~/assets/images/logo.png" width="65"/>
@@ -7,28 +7,13 @@
       <SiteNav/>
     </header>
     <main>
-      <transition name="fade" mode="out-in">
+      <transition name="fade" mode="out-in" appear>
         <router-view/>
       </transition>
     </main>
     <Footer/>
 
-    <Reveal
-      ref="revealContainer"
-      class="os_reveal"
-      @reveal-done="onRevealDone"
-    >
-    </Reveal>
-    <AnimatedSVG
-      ref="signatureContainer"
-      v-if="signatureSVG"
-      v-html="signatureSVG"
-      svgId="signature"
-      :animate="animateSignature"
-      :class="['os_signature', fillviewportClassName]"
-      @animatedsvg-done="onAnimatedSVGDone"
-    >
-    </AnimatedSVG>
+    <AnimatedOpeningScreen/>
 
   </div>
 </template>
@@ -38,20 +23,13 @@ query {
   metadata {
     siteName
   }
-  cms {
-    scalarVectorGraphics(where: {valueOfIdAttributeInSvg: "signature"}) {
-      code
-    }
-
-  }
 }
 </static-query>
 
 <script>
 import SiteNav from '~/components/SiteNav'
 import Footer from '~/components/Footer'
-import AnimatedSVG from '~/components/AnimatedSVG'
-import Reveal from '~/components/Reveal'
+import AnimatedOpeningScreen from '~/components/AnimatedOpeningScreen'
 
 import caniuse from '@/mixins/caniuse'
 
@@ -63,13 +41,10 @@ export default {
   components: {
     SiteNav,
     Footer,
-    AnimatedSVG,
-    Reveal,
+    AnimatedOpeningScreen,
   },
   data() {
     return {
-      fillviewportClassName: "fillviewport",
-      animateSignature: true,
       disableMotion: false,
     }
   },
@@ -77,69 +52,14 @@ export default {
     if (this.disableMotion) {
       this.caniuse.motion = false
     }
-    if(this.caniuse.motion) {
-      this.pageClassList.add(this.fillviewportClassName)
-      EventBus.$emit('start-animatedsvg')
-    } else {
-      this.animateSignature = false
-      this.placeSignature()
-      this.removeRevealContainer()
-    }
   },
   computed: {
-    pageEl() {
-      console.log({pagelayout:this.$refs.pagelayout})
-      return this.$refs.pagelayout
-    },
-    pageClassList() {
-      return this.pageEl.classList
-    },
-    signatureSVG() {
-      return this.$static.cms.scalarVectorGraphics[0].code
-    },
   },
   methods: {
-    onAnimatedSVGDone() {
-      EventBus.$emit('start-reveal')
-    },
-    onRevealDone() {
-      this.pageClassList.remove(this.fillviewportClassName)
-      this.removeRevealContainer()
-      this.placeSignature()
-    },
-    removeRevealContainer() {
-      this.$refs.revealContainer.$el.remove()
-    },
-    placeSignature() {
-      this.$refs.signatureContainer.$el.classList.remove(this.fillviewportClassName)
-    }
   },
 }
 </script>
 
 <style lang="less">
 @import "~/assets/styles/app.less";
-
-.app-layout.fillviewport {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden scroll;
-}
-.os_signature {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 15vw;
-  min-width: 100px;
-  transform-origin: top left;
-  transform: scale(1) translate(75px,15px);
-  transition: transform 2.5s ease;
-}
-.os_signature.fillviewport {
-  transform: scale(3) translate(5vw,5vh);
-}
-
 </style>
