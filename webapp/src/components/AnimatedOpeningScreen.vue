88c39ab4c15e8e01d570358788b0cbc4e6156990
Edward <edward.maesen@gmail.com>
2020-11-24 17:08:07 -0800

    add author tags

--- webapp/src/components/AnimatedOpeningScreen.vue
+++ webapp/src/components/AnimatedOpeningScreen.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="app-opening">
+
     <Reveal
       ref="revealContainer"
       class="os_reveal"
       @reveal-done="onRevealDone"
     >
     </Reveal>
+
     <AnimatedSVG
       ref="signatureContainer"
       v-if="signatureSVG"
@@ -16,6 +18,17 @@
       @animatedsvg-done="onAnimatedSVGDone"
     >
     </AnimatedSVG>
+
+    <div class="os_tags os_opaque" ref="tagsContainer">
+      <div
+        v-for="tag in authorTags"
+        :key="tag.name"
+        class="os_tag"
+      >
+        {{ tag.name }}
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -54,39 +67,54 @@ export default {
   data() {
     return {
       fillviewportClassName: "fillviewport",
+      heightviewportClassName: "heightviewport",
       osAnimaClassName: "os_anima",
       animateSignature: true,
     }
   },
   mounted() {
     if(this.caniuse.motion) {
-      this.containerClassList.add(this.fillviewportClassName)
+      this.rootElClassList.add(this.heightviewportClassName)
+      this.animateTags()
       EventBus.$emit('start-animatedsvg')
     } else {
       this.animateSignature = false
       this.placeSignature()
       this.removeRevealContainer()
+      this.removeTagsContainer()
     }
   },
   computed: {
-    containerClassList() {
+    rootElClassList() {
       return this.$root.$el.classList
     },
     signatureSVG() {
       return this.$static.cms.scalarVectorGraphics[0].code
     },
+    authorTags() {
+      return this.$static.cms.global.author.tag
+    }
   },
   methods: {
+    animateTags() {
+      this.$nextTick(() => {
+        this.$refs.tagsContainer.classList.add(this.osAnimaClassName)
+      })
+    },
     onAnimatedSVGDone() {
       EventBus.$emit('start-reveal')
     },
     onRevealDone() {
-      this.containerClassList.remove(this.fillviewportClassName)
+      this.rootElClassList.remove(this.heightviewportClassName)
       this.removeRevealContainer()
+      this.removeTagsContainer()
       this.placeSignature()
     },
     removeRevealContainer() {
       this.$refs.revealContainer.$el.remove()
+    },
+    removeTagsContainer() {
+      this.$refs.tagsContainer.remove()
     },
     placeSignature() {
       this.$refs.signatureContainer.$el.classList.remove(this.osAnimaClassName)
@@ -96,6 +124,9 @@ export default {
 </script>
 
 <style lang="less">
+.app-opening {
+  overflow: hidden;
+}
 .signature {
   position: absolute;
   top: 0;
@@ -104,10 +135,38 @@ export default {
   min-width: 100px;
   transform-origin: top left;
   transform: scale(1) translate(75px,15px);
-  transition: transform 2.5s ease;
+  transition: transform 2.5s ease-in-out;
 }
 .signature.os_anima {
   transform: scale(3) translate(5vw,5vh);
 }
 
+
+.os_tags {
+  position: absolute;
+  font-size: 5rem;
+  bottom: 0;
+  left: 0;
+  transform-origin: bottom left;
+  transform: scale(1) translate(9vw,-9vh);
+  transition: transform 9s ease-in-out;
+  animation: 8s ease-in-out tags;
+  opacity: 0;
+}
+.os_tags.os_anima {
+  transform: scale(1.1) translate(10vw,-10vh);
+  opacity: 0;
+}
+
+@keyframes tags {
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 </style>
