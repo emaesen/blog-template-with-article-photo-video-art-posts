8e52c97af8c9caf09cb75358d180671ce7b0aaa4
Edward <edward.maesen@gmail.com>
2020-11-24 17:08:38 -0800

    fix hor scroll bar issue

--- webapp/src/components/Reveal.vue
+++ webapp/src/components/Reveal.vue
@@ -63,8 +63,8 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
   background-color: #ccc;
   opacity: 0.97;
