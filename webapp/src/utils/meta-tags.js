8b89405bb034c56df79efe432c3d6ce81202584e
Edward <edward.maesen@gmail.com>
2020-12-01 11:20:41 -0800

    add warning if no meta tags defined

--- webapp/src/utils/meta-tags.js
+++ webapp/src/utils/meta-tags.js
@@ -2,19 +2,24 @@ import { getCmsMedia } from './medias'
 import { getSeoMetaTags } from './seo'
 
 export function getMetaTags(opts) {
-  const { title, description, shareImage, coverImage } = opts
+  if (opts) {
+    const { title, description, shareImage, coverImage } = opts
 
-  const image = shareImage && shareImage.url ? shareImage : (coverImage && coverImage.url) ? coverImage : null
+    const image = shareImage && shareImage.url ? shareImage : (coverImage && coverImage.url) ? coverImage : null
+    
+    const imageUrl = image && image.url ? getCmsMedia(image.url, false) : null
   
-  const imageUrl = image && image.url ? getCmsMedia(image.url, false) : null
-
-  if (!imageUrl) {
-    console.warn (`no SEO image found for "${title}" page!`)
+    if (!imageUrl) {
+      console.warn (`no SEO image found for "${title}" page!`)
+    }
+    const metaTags = {
+      title,
+      meta: getSeoMetaTags(title, description, imageUrl),
+    }
+    console.log({metaTags})
+    return metaTags
+  } else {
+    console.warn("No meta tags defined")
+    return null
   }
-  const metaTags = {
-    title,
-    meta: getSeoMetaTags(title, description, imageUrl),
-  }
-  console.log({metaTags})
-  return metaTags
 }
