ac2169d4f31f73d0df4a7883313c646959da433c
Edward <edward.maesen@gmail.com>
2020-12-01 15:49:45 -0800

    add arrows icon

---
+++ webapp/src/components/IconArrowUpDown.vue
@@ -0,0 +1,66 @@
+<template>
+  <svg
+    class="icon-arrows"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    @click="toggle"
+    :class="{up:isUp, down:!isUp}"
+  >
+    <g class="arrow arrow-down">
+        <path d="M10,50 L90,50 L50,90 L10,50 "/>
+    </g>
+    <g class="arrow arrow-up">
+        <path d="M10,50 L90,50 L50,10 L10,50 "/>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconArrowUpDown',
+  data() {
+    return {
+      isUp: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isUp = !this.isUp
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.icon-arrows {
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  stroke-width: 9;
+}
+.icon-arrows:hover {
+  cursor: pointer;
+}
+.arrow {
+  stroke: #000;
+  fill: #000;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  transform-origin: 50% 50%;
+  transition: all .5s ease-in-out;
+  transform: scaleY(1);
+  will-change: transform, opacity;
+  opacity: 1;
+}
+.arrow-down {
+  transform: translateY(-15px) scaleY(1)
+}
+.arrow-up {
+  transform: translateY(15px) scaleY(1)
+}
+.up .arrow-down,
+.down .arrow-up {
+  opacity: 0;
+  transform: translateY(0) scaleY(0);
+}
+</style>
