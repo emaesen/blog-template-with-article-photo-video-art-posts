9e438b12b71687d2491a76d64b6f27fd558b00e1
Edward <edward.maesen@gmail.com>
2020-12-09 13:48:38 -0800

    tweak styling

--- webapp/src/components/PostCard.vue
+++ webapp/src/components/PostCard.vue
@@ -82,14 +82,16 @@ export default {
   width: 100%;
   margin-bottom: 2em;
   line-height: normal;
-  background-color: transparent;
+  padding: 5px;
   border-radius: 5px;
-  border-bottom: 3px solid #c9c9c9;
-  background-color: #cccccc88;
-  transition: background-color .3s ease-in-out
+  border: 1px solid var(--color_border_accent-2);
+  background-color: var(--color_bg_accent-2);
+  transition: all .3s ease-in-out;
+  filter: brightness(.9);
 }
 .post-card:hover {
-  background-color: #c8ccc3aa;
+  opacity:1;
+  filter: brightness(1);
 }
 .post-title {
   margin: 0;
@@ -98,12 +100,12 @@ export default {
   position: relative;
 }
 .post-image {
-  border: 1px solid #0f5ca0;
+  border: 1px solid var(--color_border_accent-2);
   border-radius: 5px;
 }
 .post-category {
   font-size: 0.9em;
-  color: #0f5ca0d0;
+  opacity: 0.6;
   margin-right: 1em;
 }
 .post-badge {
@@ -118,7 +120,8 @@ export default {
 }
 .post-date {
   font-size: 0.8em;
-  color: #737373;
+  font-style: italic;
+  opacity: 0.8;
 }
 @media screen and (max-width: 600px) {
   .post-card {
