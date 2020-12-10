5dd390be6822662d00f542b3014c4e48e1f46697
Edward <edward.maesen@gmail.com>
2020-12-09 16:30:31 -0800

    add go back or up icon

---
+++ webapp/src/components/IconGoBackOrUp.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="action go-back-or-up" @click="goBackOrUp">⇖</span>
+</template>
+
+<script>
+import goBackOrUp from '@/mixins/go-back-or-up.js'
+
+export default {
+  name: "IconGoBackOrUp",
+  mixins: [goBackOrUp],
+}
+</script>
+
+<style lang="less" scoped>
+.go-back-or-up {
+  color: var(--color_text_action_active);
+  font-weight: 700;
+}
+</style>
