1bf3d12fdc36c1e5e17a445e0ca0379111651739
Edward <edward.maesen@gmail.com>
2020-12-10 16:24:20 -0800

    auto-generate slug as 10-char uid

--- cms/api/note/models/note.js
+++ cms/api/note/models/note.js
@@ -5,4 +5,13 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate(data) {
+      // overwrite any manually entered slug with a semi-random
+      // 10-character id
+      // using a simple date-based id generator
+      data.slug = ((new Date()).getTime().toString(36) + Math.random().toString(36).slice(2)).substring(2,12)
+    },
+  },
+};
