55e83e6bca270733aa4d9087adf97937bbabcd7d
Edward <edward.maesen@gmail.com>
2020-12-11 16:13:06 -0800

    move and rename style class names

--- webapp/src/pages/404.vue
+++ webapp/src/pages/404.vue
@@ -3,9 +3,9 @@
     <div class="center anima__zoom pnf">
       <p class="anima__flicker emph spaced_chars">PAGE NOT FOUND</p>
       <p class="spaced_chars">---------</p>
-      <p class="anima__slide-in-from-below delay1 spaced_chars">SO</p>
-      <p class="anima__slide-in-from-below delay2 spaced_chars">SORRY</p>
-      <p class="anima__slide-in-from-below delay3 spacious">
+      <p class="anima__slide-in-from-below anima__-delay1 spaced_chars">SO</p>
+      <p class="anima__slide-in-from-below anima__-delay2 spaced_chars">SORRY</p>
+      <p class="anima__slide-in-from-below anima__-delay3 spacious">
         The page you requested does not –or no longer– exist.<br>
         Please click any of the navigation links to continue.<br>
       </p>
@@ -16,17 +16,5 @@
 <style lang="less" scoped>
 .pnf {
   margin: 10vh 0 60vh 0;
-}
-.spaced_chars {
-  letter-spacing: .7em;
-}
-.delay1 {
-  animation-delay: 1s;
-}
-.delay2 {
-  animation-delay: 1.1s;
-}
-.delay3 {
-  animation-delay: 2s;
 }
 </style>
