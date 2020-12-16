ad1460b8d5843ec8dd027021961df801f40b0cfe
Edward <edward.maesen@gmail.com>
2020-12-15 16:28:47 -0800

    add latest photos component

---
+++ webapp/src/components/LatestPhotos.vue
@@ -0,0 +1,113 @@
+<template>
+  <section v-if="hasPhotos" class="h-feed">
+    <div class="cards-header-container">
+      <h2 class="cards-header p-name">
+        Latest photos
+      </h2>
+      <a 
+        v-if="showViewAllLink"
+        class="button" href="#"
+      >view all {{ totalNrOfPhotos }} photos</a>
+    </div>
+    <div class="cards-container">
+      <PostCard
+        v-for="photo in latestPhotos"
+        :key="photo.id"
+        :post="photo"
+        postType="photo"
+      />
+    </div>
+  </section>
+</template>
+
+<static-query>
+query LatestPhotos($limit: Int = 4) {
+  cms {
+    # List photos
+    latestPhotos: photos(sort: "createdAt:desc" limit:$limit) {
+      id
+      title
+      slug
+      categories {
+        id
+        title
+      }
+      series {
+        id
+        title
+      }
+      photo {
+        id
+        url
+      }
+      date
+      location {
+        landmark
+        city
+        state_province
+        country
+      }
+    }
+    allPhotos: photos {
+    	slug
+    }
+  }
+}
+</static-query>
+
+<script>
+import PostCard from '~/components/PostCard'
+import RichText from '~/components/RichText'
+import { getCmsMedia } from '~/utils/medias'
+import { getSeoMetaTags } from '~/utils/seo'
+
+export default {
+  methods: {
+    getCmsMedia,
+  },
+  components: {
+    PostCard,
+    RichText,
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    latestPhotos() {
+      // the maximum nr of photos to show is defined by `limit` in the 
+      // graphql query
+      return this.$static.cms.latestPhotos
+    },
+    nrOfDisplayedPhotos() {
+      return this.latestPhotos.length
+    },
+    hasPhotos() {
+      return this.nrOfDisplayedPhotos > 0
+    },
+    totalNrOfPhotos() {
+      return this.$static.cms.allPhotos.length
+    },
+    showViewAllLink() {
+      return this.totalNrOfPhotos > this.nrOfDisplayedPhotos
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.cards-header-container {
+  border-bottom: 1px solid var(--color_border_accent-2);
+  margin-bottom: 1.5em;
+}
+.cards-header {
+  display: inline-block;
+  margin-bottom: 0;
+}
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  place-content: space-between;
+}
+
+</style>
