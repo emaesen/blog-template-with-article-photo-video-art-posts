a833a25361761e899d2b7349ce0adfc62323856c
Edward <edward.maesen@gmail.com>
2021-01-12 16:25:13 -0800

    add arts page single type

---
+++ cms/api/arts-page/controllers/arts-page.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+// `sanitizeEntity` removes all private fields from the model 
+// and its relations
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+
+  async find(ctx) {
+    console.log("find ArtsPage")
+    const entity = await strapi.services['arts-page'].find();
+    return sanitizeEntity(entity, { model: strapi.models['arts-page'] });
+  },
+};
