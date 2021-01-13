c3ef5bbf13b67e1afd903113e42799ebbe6d8baf
Edward <edward.maesen@gmail.com>
2021-01-13 11:57:49 -0800

    add new art templates

---
+++ webapp/src/templates/Art.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="main-art h-entry as-art">
+    
+    <h1>
+      <span  @click="goBackOrUp">
+        <IconGoBackOrUp :title="goBackOrUpText"/>
+      </span>
+      {{ art.title }}
+    </h1>
+    <div class="date dt-published">
+      {{ datePublishedText }}
+    </div>
+
+
+    <g-image
+      :alt="art.title"
+      :src="imgUrl"
+    />
+    <div class="meta deemph">
+      <div class="date dt-taken">
+        {{ dateCreatedText }}
+      </div>
+      <div class="categories">
+        <span
+          v-for="category in art.categories"
+          :key="category.id"
+          class="category p-category post-category"
+        >
+          <g-link :to="categoryBasePath + category.title" class="nodeco">
+            #{{ category.title }}
+          </g-link>
+        </span>
+      </div>
+      <div v-if="seriesTitle" class="series">
+        Series: 
+        <g-link :to="seriesBasePath + seriesTitle" class="nodeco post-series">
+             ❈ {{ seriesTitle }}
+          </g-link>
+      </div>
+    </div>
+
+    <div class="description para spacious">
+      {{ art.description }}
+    </div>
+  </div>
+</template>
+
+<page-query>
+query Art ($slug: String!) {
+  cms {
+    arts(where: { slug: $slug }) {
+      id
+      title
+      slug
+      description
+      categories {
+        id
+        title
+      }
+      series {
+        id
+        title
+      }
+      image {
+        id
+        url
+      }
+      date
+      createdAt
+    }
+  }
+}
+</page-query>
+
+<script>
+import IconGoBackOrUp from '~/components/IconGoBackOrUp'
+import Content from '~/components/Content'
+import { getCmsMedia } from '~/utils/medias'
+import { getMetaTags } from '~/utils/meta-tags'
+import date from '@/mixins/date.js'
+import goBackOrUp from '@/mixins/go-back-or-up.js'
+
+
+export default {
+  name: 'Art',
+  mixins: [date, goBackOrUp],
+  components: {
+    IconGoBackOrUp,
+    Content,
+  },
+  mounted() {
+  },
+  metaInfo() {
+    return getMetaTags(this.art) 
+  },
+  computed: {
+    art() {
+      return this.$page.cms.arts[0]
+    },
+    imgUrl() {
+      const url=this.art.image.url
+      return getCmsMedia(url)
+    },
+    categoryBasePath() {
+      return '/p/arts/c/'
+    },
+    seriesBasePath() {
+      return '/p/arts/s/'
+    },
+    seriesTitle() {
+      const series = this.art && this.art.series
+      return series && series.title
+    },
+    dateCreatedText() {
+      let opts = {shortForm:true, showYear:true};
+      let text = this.formattedDate(this.art.date, opts);
+      return text;
+    },
+    datePublishedText() {
+      let opts = {shortForm:true, showYear:true};
+      let text = this.formattedDate(this.art.createdAt, opts);
+      return text;
+    },
+  },
+  methods: {
+    getCmsMedia,
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.dt-published {
+  position: relative;
+  top: -3em;
+  font-style: italic;
+  font-size: 0.9em;
+  opacity: 0.8;
+}
+.meta {
+  text-align: center;
+  font-style: italic;
+}
+.category {
+  font-size: 0.9em;
+  opacity: 0.6;
+  margin: 0 1em;
+}
+</style>
---
+++ webapp/src/templates/Arts.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="main-arts">
+    <h1>{{artsPage.title}}</h1>
+
+    <g-image
+      v-if="introImage"
+      class="img-postcat"
+      alt="arts posts"
+      :src="introImage"
+    />
+
+    <RichText :data="artsPage.introduction" class="para intro group"/>
+
+    <div class="h-feed">
+      <div class="cards-container">
+        <PostCard
+          v-for="art in arts"
+          :key="art.id"
+          :post="art"
+          postType="art"
+        />
+      </div>
+    </div>
+    <Pagination 
+      :basePath="basePath"
+      :currentPage="currentPage"
+      :totalPages="totalPages"
+    />
+
+  </div>
+</template>
+
+<script>
+import Pagination from '~/components/Pagination'
+import PostCard from '~/components/PostCard'
+import RichText from '~/components/RichText'
+import { getCmsMedia } from '~/utils/medias'
+import { getMetaTags } from '~/utils/meta-tags'
+
+export default {
+  name: 'Arts',
+  methods: {
+  },
+  components: {
+    Pagination,
+    PostCard,
+    RichText,
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    console.log(this.$context)
+  },
+  metaInfo() {
+    return getMetaTags(this.artsPage.seo) 
+  },
+  computed: {
+    artsPage() {
+      return this.$page.cms.artsPage
+    },
+    arts() {
+      // the maximum nr of arts to show is defined by `limit` in the 
+      // graphql query
+      console.log({arts:this.$page.cms.arts})
+      return this.$page.cms.arts
+    },
+    introImage() {
+      const img = this.artsPage.seo.shareImage
+      return getCmsMedia(img && img.url)
+    },
+    basePath() {
+      return this.$route.path.replace(/\/\d+.*/,"/");
+    },
+    currentPage() {
+      return this.$context.page
+    },
+    totalPages() {
+      return this.$context.totalPages
+    }
+  }
+}
+</script>
+
+<page-query>
+query ArtsPage ($sort: String!, $start: Int, $limit: Int) {
+  cms {
+    artsPage {
+      title
+      introduction
+      seo {
+        title
+        description
+        shareImage {
+          id
+          url
+        }
+      }
+    }
+
+    # List arts
+    arts (sort: $sort, limit:$limit, start:$start) {
+      id
+      title
+      slug
+      categories {
+        id
+        title
+      }
+      series {
+        id
+        title
+      }
+      image {
+        id
+        url
+      }
+      date
+    }
+
+  }
+}
+</page-query>
+
+<style lang="less" scoped>
+</style>
