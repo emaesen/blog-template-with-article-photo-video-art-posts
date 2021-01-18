6686691194a8acfe0a3eb81cf99b96f73ac9a0ea
Edward <edward.maesen@gmail.com>
2021-01-18 10:17:05 -0800

    add toTitleCase method

--- webapp/src/mixins/taxonomy.js
+++ webapp/src/mixins/taxonomy.js
@@ -66,6 +66,9 @@ export default {
     },
     reverseSort() {
       this.showLatestOnTop = !this.showLatestOnTop
+    },
+    toTitleCase(str) {
+      return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
   },
 }
