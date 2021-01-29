a84aea6194bd7a4efbf9053e931d5f895abdc246
Edward <edward.maesen@gmail.com>
2021-01-28 16:14:04 -0800

    tweak styling

--- webapp/src/pages/Biography.vue
+++ webapp/src/pages/Biography.vue
@@ -10,25 +10,30 @@
        {{ bio.introduction }}
     </div>
 
-    <div class="biography-cards-container">
-      <BiographyCard
-        v-for="bioItem in bioItems"
-        :key="bioItem.id"
-        :bioItem="bioItem"
-      />
+    <div class="biography-timeline timeline">
+      <div class="timeline-bar"></div>
+      <ul>
+        <BiographyItem
+          v-for="bioItem in bioItems"
+          :key="bioItem.id"
+          :bioItem="bioItem"
+        />
+      </ul>
+
+      
     </div>
   </div>
 </template>
 
 <script>
-import BiographyCard from '~/components/BiographyCard'
+import BiographyItem from '~/components/BiographyItem'
 import { getMetaTags } from '~/utils/meta-tags'
 
 
 export default {
   name: 'Biography',
   components: {
-    BiographyCard,
+    BiographyItem,
   },
   metaInfo() {
     return getMetaTags(this.bio.seo, this.$route) 
@@ -72,3 +77,69 @@ query BiographyPage {
   }
 }
 </page-query>
+
+<style lang="less">
+.timeline {
+  position: relative;
+}
+.timeline-bar {
+  padding: 0;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 50vw;
+  width: 1px;
+  background-color: var(--color_border_accent-1);
+}
+.timeline ul {
+  margin: 0;
+  margin-block: 0 0;
+  padding-inline-start: 0;
+}
+.timeline li {
+	position:relative;
+	margin: 1em 0;
+	width:45vw;
+  list-style-type:none;
+  padding: .5em;
+  &:before, &:after {
+    position: absolute;
+    display: inline-block;
+    top: .9em;
+    content: '';
+    border-radius: .5em;
+    width: .5em;
+    height: .5em;
+  }
+}
+.timeline li:nth-child(odd) {
+  left: 5vw;
+  text-align: right;
+  &:after {
+    border: 1px solid var(--color_border_accent-1);
+    background-color: var(--color_bg);
+    right: -.25em;
+  }
+  .biography-when {
+    left: 47vw;
+    text-align: left;
+  }
+}
+.timeline li:nth-child(even) {
+  left:50vw;
+  &:before {
+    border: 1px solid var(--color_border_accent-1);
+    background-color: var(--color_bg);
+    left: -.25em;
+  }
+  .biography-when {
+    right: 47vw;
+    text-align: right;
+  }
+}
+
+.biography-when {
+  position: absolute;
+  top: .7em;
+}
+</style>
