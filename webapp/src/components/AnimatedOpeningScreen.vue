8f701c6596b77909b8df860a1a547d61ebaffa78
Edward <edward.maesen@gmail.com>
2021-03-08 16:41:58 -0800

    tweak behavior of animated opening screen

--- webapp/src/components/AnimatedOpeningScreen.vue
+++ webapp/src/components/AnimatedOpeningScreen.vue
@@ -14,7 +14,7 @@
       v-html="signatureSVG"
       svgId="signature"
       :animate="animateSignature"
-      :class="['signature', osAnimaClassName]"
+      class="signature"
       @animatedsvg-done="onAnimatedSVGDone"
     >
     </AnimatedSVG>
@@ -68,6 +68,7 @@ export default {
     return {
       fillviewportClassName: "fillviewport",
       heightviewportClassName: "heightviewport",
+      osAnimaTransClassName: "os_anima_trans",
       osAnimaClassName: "os_anima",
       animateSignature: true,
     }
@@ -97,6 +98,9 @@ export default {
     authorTags() {
       return this.global && this.global.author && this.global.author.tag
     },
+    signatureContainer() {
+      return this.$refs.signatureContainer.$el;
+    },
     tagsContainer() {
       return this.$refs.tagsContainer;
     }
@@ -108,11 +112,16 @@ export default {
       if(this.caniuse.motion && this.$route.name !== "*" && this.signatureSVG) {
         this.rootElClassList.add(this.heightviewportClassName)
         this.animateTags()
+        this.signatureContainer.classList.add(this.osAnimaClassName)
+        // Trigger layout to set startpoint styles
+        this.signatureContainer.getBoundingClientRect()
+        this.signatureContainer.classList.add(this.osAnimaTransClassName)
         EventBus.$emit('start-animatedsvg')
       } else {
         this.animateSignature = false
         if(this.signatureSVG) {
           this.placeSignature()
+          EventBus.$emit('start-animatedsvg')
         }
         this.removeRevealContainer()
         this.removeTagsContainer()
@@ -120,7 +129,12 @@ export default {
     },
     animateTags() {
       this.$nextTick(() => {
+        // Trigger layout to set startpoint styles for SSR case
+        this.tagsContainer.getBoundingClientRect()
         this.tagsContainer.classList.add(this.osAnimaClassName)
+        // just in case animation did not run properly, force remove the
+        // tags container after 15 seconds
+        setTimeout(this.removeTagsContainer, 15 * 1000)
       })
     },
     onAnimatedSVGDone() {
@@ -138,7 +152,7 @@ export default {
       this.tagsContainer.remove()
     },
     placeSignature() {
-      this.$refs.signatureContainer.$el.classList.remove(this.osAnimaClassName)
+      this.signatureContainer.classList.remove(this.osAnimaClassName)
     }
   },
 }
@@ -156,28 +170,30 @@ export default {
   max-width: 180px;
   transform-origin: top left;
   transform: scale(1) translate(75px,7px);
-  transition: transform 2.5s ease-in-out;
   z-index: 9;
 }
-.js-yes .signature.os_anima {
+.signature.os_anima_trans {
+  transition: transform 2.5s ease-in-out;
+}
+.signature.os_anima {
   transform: scale(2.7) translate(3vw,4vh);
 }
 
 
 .os_tags {
   position: absolute;
-  font-size: 3.69rem;
+  font-size: 3rem;
   bottom: 0;
   left: 0;
   transform-origin: bottom left;
-  transform: scale(1) translate(1vw,-9vh);
+  transform: scale(1) translate(1vw,-6vh);
   transition: transform 9s ease-in-out;
   animation: 9s ease-in-out tags;
   opacity: 0;
   z-index: 2;
 }
 .os_tags.os_anima {
-  transform: scale(1.1) translate(2vw,-10vh);
+  transform: scale(.6) translate(90vw,-50vh);
   opacity: 0;
 }
 
@@ -186,7 +202,7 @@ export default {
     opacity: 0;
   }
   30% {
-    opacity: 0.5;
+    opacity: 0.3;
   }
   100% {
     opacity: 0;
