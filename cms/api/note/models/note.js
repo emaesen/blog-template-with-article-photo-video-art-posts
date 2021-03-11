25e9bd5629d9c2a9aceb543d1215e0eb6638da5a
Edward <edward.maesen@gmail.com>
2021-03-11 09:52:57 -0800

    modify note slug to allow simpler redirect rule

--- cms/api/note/models/note.js
+++ cms/api/note/models/note.js
@@ -8,10 +8,10 @@
 module.exports = {
   lifecycles: {
     beforeCreate(data) {
-      // overwrite any manually entered slug with a semi-random
-      // 10-character id
+      // overwrite any manually entered slug with "note-"
+      // appended with a semi-random 10-character id
       // using a simple date-based id generator
-      data.slug = ((new Date()).getTime().toString(36) + Math.random().toString(36).slice(2)).substring(2,12)
+      data.slug = "note-"+((new Date()).getTime().toString(36) + Math.random().toString(36).slice(2)).substring(2,12)
     },
   },
 };
