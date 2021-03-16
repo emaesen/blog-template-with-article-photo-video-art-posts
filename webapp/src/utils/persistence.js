1b62e671f5268d43ce48cfc7fd506422c308a6e0
Edward <edward.maesen@gmail.com>
2021-03-16 12:48:53 -0700

    add opening-animation-shown and refactor

--- webapp/src/utils/persistence.js
+++ webapp/src/utils/persistence.js
@@ -1,65 +1,116 @@
 /* very basic persistence logic */
 
-const store = (typeof window !== 'undefined') && window.localStorage;
-const colorModeIndex_Key = "epvm:color-mode-index";
-const selectionIndex_Key = "epvm:selection-index";
+const localStore = (typeof window !== 'undefined') && window.localStorage
+const sessionStore = (typeof window !== 'undefined') && window.sessionStorage
+const colorModeIndex_Key = "epvm:color-mode-index"
+const selectionIndex_Key = "epvm:selection-index"
+const openingAnimationShown_Key = "epvm:opening-animation-shown"
 
+/* generic functions */
 
-function persistData(key, value) {
-  store && store.setItem(key, value);
+function persistSessionData(key, value) {
+  sessionStore && sessionStore.setItem(key, value)
 }
 
-function retrieveData(key) {
-  return store && store.getItem(key);
+function retrieveSessionData(key) {
+  return sessionStore && sessionStore.getItem(key)
 }
 
-function clearData(key) {
-  return store && store.removeItem(key);
+function clearSessionData(key) {
+  return sessionStore && sessionStore.removeItem(key)
 }
+
+function persistLocalData(key, value) {
+  localStore && localStore.setItem(key, value)
+}
+
+function retrieveLocalData(key) {
+  return localStore && localStore.getItem(key)
+}
+
+function clearLocalData(key) {
+  return localStore && localStore.removeItem(key)
+}
+
+function ensureNumber(val, curVal) {
+  if (val !== null) {
+    return 1 * val
+  } else {
+    return curVal
+  }
+}
+
+function ensureBoolean(val, curVal) {
+  if (val !== null) {
+    if (val===1 || val==="true" || val===true) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return curVal
+  }
+}
+
+/* get/set color mode index for light-dark color scheme */
 
 export function persistColorModeIndex(value) {
-  persistData(colorModeIndex_Key, value) ;
+  persistLocalData(colorModeIndex_Key, value)
 }
 
 export function retrieveColorModeIndex(curVal) {
   if (curVal === undefined) {
-    return retrieveData(colorModeIndex_Key);
+    return retrieveLocalData(colorModeIndex_Key)
   } else {
     const persistedColorModeIndex = retrieveColorModeIndex()
-    if (persistedColorModeIndex !== null) {
-      return persistedColorModeIndex
-    } else {
-      return curVal
-    }
+    return ensureNumber(persistedColorModeIndex, curVal)
   }
 }
 
 export function clearColorModeIndex() {
-  clearData(colorModeIndex_Key) ;
+  clearLocalData(colorModeIndex_Key)
 }
 
+
+/* get/set selection index for featured/latest posts display */
 
 function compoundKey(key, subkey) {
   return key + "--" + subkey
 }
 
 export function persistSelectionIndex(subkey, value) {
-  persistData(compoundKey(selectionIndex_Key, subkey), value) ;
+  persistLocalData(compoundKey(selectionIndex_Key, subkey), value) 
 }
 
 export function retrieveSelectionIndex(subkey, curVal) {
   if (curVal === undefined) {
-    return retrieveData(compoundKey(selectionIndex_Key, subkey));
+    return retrieveLocalData(compoundKey(selectionIndex_Key, subkey))
   } else {
     const persistedSelectionIndex = retrieveSelectionIndex(subkey)
-    if (persistedSelectionIndex !== null) {
-      return 1 * persistedSelectionIndex
-    } else {
-      return curVal
-    }
+    return ensureNumber(persistedSelectionIndex, curVal)
   }
 }
 
 export function clearSelectionIndex(subkey) {
-  clearData(compoundKey(selectionIndex_Key, subkey)) ;
+  clearLocalData(compoundKey(selectionIndex_Key, subkey))
+}
+
+
+/* get/set openingAnimationShown indicator */
+
+export function persistOpeningAnimationShown(value) {
+  persistSessionData(openingAnimationShown_Key, value)
+}
+
+export function retrieveOpeningAnimationShown(curVal) {
+  if (curVal === undefined) {
+    return retrieveSessionData(openingAnimationShown_Key)
+  } else {
+    const persistedOpeningAnimationShown = retrieveOpeningAnimationShown()
+    return ensureBoolean(persistedOpeningAnimationShown, curVal)
+  }
+}
+
+export function clearOpeningAnimationShown() {
+  clearSessionData(openingAnimationShown_Key)
 }
